9c0607198c2b1e3aa4ade986256e8d81e44f9836
Rebeccah5 <suzieatuheire@gmail.com>
2020-09-11 01:48:06 +0300

    views,templates $ product details

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import product_list
+from .views import product_list, details
 
 urlpatterns = [
     path('templates/', product_list, name='product_list'),
+    path('products/<int :product_id>/',details,name='details')
 
 ]
--- catalogue/views.py
+++ catalogue/views.py
@@ -6,7 +6,11 @@ from .models import Product
 def product_list(request):
     product = Product.objects.all()
     return render(request, 'product_list.html', {'product': product})
-    
+
+def details(request,product_id):
+    product=Product.objects.get(id=product_id) 
+    images=product.images.all()  
+    return render(request,'details.html', {'product':product,'images':images})
     
  
 
